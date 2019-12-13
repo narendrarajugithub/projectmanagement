c2de7ad4259a33a718d81fe8d2ff4fd20ed466e5
narendrarajugithub <ynarendraraju@gmail.com>
2019-12-14 00:06:36 +0530

    Revert "vd"
    This reverts commit 933e9c32ad9aa8013b935f5bcd109cec6d450754.

--- projectmanagement/src/main/java/com/ust/pms/dao/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ust.pms.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ust.pms.attribute.Project;
-
-public interface ProjectRepository extends CrudRepository<Project, Long> {
-/**/
-}

933e9c32ad9aa8013b935f5bcd109cec6d450754
narendrarajugithub <ynarendraraju@gmail.com>
2019-12-14 00:00:14 +0530

    vd

---
+++ projectmanagement/src/main/java/com/ust/pms/dao/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.ust.pms.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ust.pms.attribute.Project;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+/**/
+}

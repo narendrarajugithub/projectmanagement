a0bac8bed3318ec7f5e1ff700d13bf52b360e27a
narendrarajugithub <ynarendraraju@gmail.com>
2020-01-05 18:02:36 +0530

    commit

--- src/main/java/com/ust/pms/controller/HomeController.java
+++ src/main/java/com/ust/pms/controller/HomeController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ust.pms.dao.EmployeeRepository;
 import com.ust.pms.dao.ProjectDao;
+import com.ust.pms.entity.Employee;
 import com.ust.pms.entity.Project;
 
 
@@ -16,10 +18,15 @@ import com.ust.pms.entity.Project;
 public class HomeController {
 	@Autowired
 	ProjectDao projrepo;
+	
+	@Autowired
+	EmployeeRepository emprepo;
+	
 	@GetMapping("/")
 	public String diplayHome(Model model) {
 	List <Project> projects=  projrepo.findAll();
-	
+	List<Employee> employees =emprepo.findAll();
+	model.addAttribute("employees",employees);
 	model.addAttribute("projects", projects);
 	return "main/home";
 	}
